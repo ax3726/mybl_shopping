4bcb88c84a0075e7c0ac9118b64078198ebe4077
ax3726@163.com <15970060419lma>
2018-08-23 11:20:37 +0800

    修改订单

--- app/src/main/java/com/ycblsc/ui/mine/ShoppingOrderListActivity.java
+++ app/src/main/java/com/ycblsc/ui/mine/ShoppingOrderListActivity.java
@@ -1,5 +1,6 @@
 package com.ycblsc.ui.mine;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -23,6 +24,7 @@ import com.ycblsc.databinding.ActivityShoppingOrderListBinding;
 import com.ycblsc.model.BaseBean;
 import com.ycblsc.model.shopping.ShoppingOrderListModel;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,7 +34,8 @@ public class ShoppingOrderListActivity extends BaseActivity<BasePresenter, Activ
     private CommonAdapter<ShoppingOrderListModel.ReturnDataBean> mAdapter;
     private int mPage = 1;
     private int mSize = 10;
-    private int mType=0;
+    private int mType = 0;
+
     @Override
     protected boolean isTitleBar() {
         return true;
@@ -41,7 +44,13 @@ public class ShoppingOrderListActivity extends BaseActivity<BasePresenter, Activ
     @Override
     protected void initTitleBar() {
         super.initTitleBar();
-        mTitleBarLayout.setTitle("订单信息");
+        mType = getIntent().getIntExtra("type", 0);
+        if (mType == 0) {
+            mTitleBarLayout.setTitle("我的订单");
+        } else {
+            mTitleBarLayout.setTitle("限时达消费记录");
+        }
+
     }
 
     @Override
@@ -62,14 +71,16 @@ public class ShoppingOrderListActivity extends BaseActivity<BasePresenter, Activ
     @Override
     protected void initData() {
         super.initData();
-        mType=getIntent().getIntExtra("type",0);
+
         if (CacheService.getIntance().isLogin()) {
             initAdapter();
             getData();
         }
     }
 
-    private void initAdapter() {
+    DecimalFormat df = new DecimalFormat("0.00");
+
+    private void adapter1() {
         mAdapter = new CommonAdapter<ShoppingOrderListModel.ReturnDataBean>(this, R.layout.item_shopping_order_list, mDataList) {
             @Override
             protected void convert(ViewHolder holder, ShoppingOrderListModel.ReturnDataBean dataBean, int position) {
@@ -77,24 +88,67 @@ public class ShoppingOrderListActivity extends BaseActivity<BasePresenter, Activ
                 TextView tv_min = holder.getView(R.id.tv_min);
                 TextView tv_order_time = holder.getView(R.id.tv_order_time);
                 TextView tv_send_time = holder.getView(R.id.tv_send_time);
+                TextView tv_send_time_hint = holder.getView(R.id.tv_send_time_hint);
+                ImageView img_send_time = holder.getView(R.id.img_send_time);
+                ImageView img_ok_time = holder.getView(R.id.img_ok_time);
+                ImageView img_pay_con = holder.getView(R.id.img_pay_con);
                 TextView tv_ok_time = holder.getView(R.id.tv_ok_time);
+                TextView tv_ok_time_hint = holder.getView(R.id.tv_ok_time_hint);
                 TextView tv_pay_con = holder.getView(R.id.tv_pay_con);
-                LinearLayout lly_ok = holder.getView(R.id.lly_ok);
-                LinearLayout lly_ok_time = holder.getView(R.id.lly_ok_time);
-                LinearLayout lly_pay_time = holder.getView(R.id.lly_pay_time);
+                TextView tv_pay_con_hint = holder.getView(R.id.tv_pay_con_hint);
+                TextView tv_time_day = holder.getView(R.id.tv_time_day);
+                TextView tv_moeny = holder.getView(R.id.tv_money1);
+                TextView tv_qian = holder.getView(R.id.tv_qian);
+                TextView tv_money_count = holder.getView(R.id.tv_money_count);
+
+
                 ImageView img_ok = holder.getView(R.id.img_ok);
+
+                tv_time_day.setText(TextUtils.isEmpty(dataBean.getStartTime()) ? "不详" : dataBean.getStartTime().split(" ")[0]);
+
+                tv_moeny.setText(dataBean.getOrderMoney());
+                tv_money_count.setText(dataBean.getOrderMoney());
 
                 tv_min.setText(dataBean.getMaxTime());
                 tv_order_time.setText(dataBean.getStartTime());
-                tv_send_time.setText(dataBean.getSendTime());
 
+                if (TextUtils.isEmpty(dataBean.getSendTime())) {
+                    img_send_time.setVisibility(View.INVISIBLE);
+                    tv_send_time.setText("");
+                    tv_send_time_hint.setTextColor(Color.parseColor("#ACA7A7"));
+                } else {
+                    img_send_time.setVisibility(View.VISIBLE);
+                    tv_send_time.setText(dataBean.getSendTime());
+                    tv_send_time_hint.setTextColor(Color.parseColor("#1F1F1F"));
+                }
 
-             /*   lly_ok_time.setVisibility(TextUtils.isEmpty(dataBean.getCspf())?View.GONE : View.VISIBLE);
-                lly_pay_time.setVisibility(TextUtils.isEmpty(dataBean.getGetTime())?View.GONE : View.VISIBLE);*/
-                tv_pay_con.setText(dataBean.getCspf());
-                tv_ok_time.setText(dataBean.getGetTime());
+                if (TextUtils.isEmpty(dataBean.getGetTime())) {
+                    img_ok_time.setVisibility(View.INVISIBLE);
+                    tv_ok_time.setText("");
+                    tv_ok_time_hint.setTextColor(Color.parseColor("#ACA7A7"));
+                } else {
+                    img_ok_time.setVisibility(View.VISIBLE);
+                    tv_ok_time.setText(dataBean.getGetTime());
+                    tv_ok_time_hint.setTextColor(Color.parseColor("#1F1F1F"));
+                }
+                if (TextUtils.isEmpty(dataBean.getCspf())) {
+                    img_pay_con.setVisibility(View.INVISIBLE);
+                    tv_pay_con.setText("");
+                    tv_pay_con_hint.setTextColor(Color.parseColor("#ACA7A7"));
+                } else {
+                    img_pay_con.setVisibility(View.VISIBLE);
+                    tv_pay_con.setText(dataBean.getCspf());
+                    tv_pay_con_hint.setTextColor(Color.parseColor("#1F1F1F"));
+                }
 
-                lly_ok.setVisibility("010704".equals(dataBean.getStatus()) ? View.VISIBLE : View.GONE);
+                if ("010704".equals(dataBean.getStatus())) {//待确认
+                    tv_qian.setTextColor(Color.parseColor("#1F1F1F"));
+                    img_ok.setEnabled(true);
+                } else {
+                    tv_qian.setTextColor(Color.parseColor("#ACA7A7"));
+                    img_ok.setEnabled(false);
+                }
+
 
                 img_ok.setOnClickListener(new View.OnClickListener() {
                     @Override
@@ -122,6 +176,55 @@ public class ShoppingOrderListActivity extends BaseActivity<BasePresenter, Activ
                 rc_item.setAdapter(adapter);
             }
         };
+    }
+
+    private void adapter2() {
+        mAdapter = new CommonAdapter<ShoppingOrderListModel.ReturnDataBean>(this, R.layout.item_shopping_order_list1, mDataList) {
+            @Override
+            protected void convert(ViewHolder holder, ShoppingOrderListModel.ReturnDataBean dataBean, int position) {
+                RecyclerView rc_item = holder.getView(R.id.rc_item);
+                TextView tv_min = holder.getView(R.id.tv_min);
+                TextView tv_order_time = holder.getView(R.id.tv_order_time);
+                TextView tv_money_count = holder.getView(R.id.tv_money_count);
+                TextView tv_tiyan = holder.getView(R.id.tv_tiyan);
+                TextView tv_chao = holder.getView(R.id.tv_chao);
+                TextView tv_money = holder.getView(R.id.tv_money);
+
+                tv_order_time.setText(dataBean.getStartTime());
+                tv_tiyan.setText(dataBean.getShopName());
+                tv_chao.setText(dataBean.getCspf());
+                tv_min.setText(dataBean.getMaxTime() + "分钟");
+                tv_money_count.setText("￥" + dataBean.getOrderMoney());
+                tv_money.setText("￥" + dataBean.getOrderMoney());
+
+
+                List<ShoppingOrderListModel.ReturnDataBean.SdBean> sd = dataBean.getSd();
+                if (sd == null) {
+                    sd = new ArrayList<>();
+                }
+                CommonAdapter<ShoppingOrderListModel.ReturnDataBean.SdBean> adapter
+                        = new CommonAdapter<ShoppingOrderListModel.ReturnDataBean.SdBean>(aty, R.layout.item_shopping_order_list_child, sd) {
+                    @Override
+                    protected void convert(ViewHolder holder, ShoppingOrderListModel.ReturnDataBean.SdBean item, int position) {
+                        holder.setImageurl(R.id.img, item.getImg(), 0);
+                        holder.setText(R.id.tv_shopname, item.getS_name());
+                        holder.setText(R.id.tv_moeny, "￥" + item.getProPrice());
+                        holder.setText(R.id.tv_count, "x" + item.getProCount());
+                        holder.setText(R.id.tv_total, "总计:￥" + item.getProSum());
+                    }
+                };
+                rc_item.setLayoutManager(new LinearLayoutManager(aty));
+                rc_item.setAdapter(adapter);
+            }
+        };
+    }
+
+    private void initAdapter() {
+        if (mType == 0) {
+            adapter1();
+        } else {
+            adapter2();
+        }
         mBinding.rcBody.setLayoutManager(new LinearLayoutManager(this));
         mBinding.rcBody.setAdapter(mAdapter);
         mBinding.srlBody.setOnRefreshLoadmoreListener(new OnRefreshLoadmoreListener() {
@@ -159,7 +262,7 @@ public class ShoppingOrderListActivity extends BaseActivity<BasePresenter, Activ
     }
 
     private void getData() {
-        Api.getApi2().getLoadOrderList(CacheService.getIntance().getUserId(), mPage, mSize, mType==0?"":"010705")
+        Api.getApi2().getLoadOrderList(CacheService.getIntance().getUserId(), mPage, mSize, mType == 0 ? "" : "010705")
                 .compose(callbackOnIOToMainThread())
                 .subscribe(new BaseNetSubscriber<ShoppingOrderListModel>(true) {
                     @Override
